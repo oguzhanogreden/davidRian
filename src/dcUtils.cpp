c2eee0258485cebc0c1bff632c217865de68785b
Oğuzhan Öğreden <oguzhan@oguzhanogreden.com>
2018-01-19 11:12:19 +0100

    comments cpp

--- src/dcUtils.cpp
+++ src/dcUtils.cpp
@@ -6,6 +6,9 @@
 // using namespace Rcpp;
 
 arma::mat expVec(double x, int deg) {
+  // This is the third term in the polynomial.
+  // Simply a column vector containing the exponential serie
+  // 1 + theta + theta^2 + ... + theta_k.
   arma::mat out(deg+1, 1);
   
   for (int i = 0; i < deg+1; i++) {
@@ -16,6 +19,9 @@ arma::mat expVec(double x, int deg) {
 }
 
 arma::mat cMat (int k, NumericVector phi) {
+  // c is one of the central terms in Equation (9) of Woods & Lin (2009).
+  // This function creates the c matrix given a degree of k and a vector phi of
+  // length k, containing the parameters of the polynomial P_k^2(\theta).
   arma::mat prim_c(1,1);
   arma::mat c(1, 1);
   
@@ -30,34 +36,34 @@ arma::mat cMat (int k, NumericVector phi) {
       if (i + 1 == prim_c.n_rows) { // if last row
         // write all cos's
         for (int j = 0; j < prim_c.n_cols; j++) {
-        prim_c(i, j) = cos(phi[j]);
+          prim_c(i, j) = cos(phi[j]);
         }
       } else { // if not last row
         for (int j = 0; j < prim_c.n_cols; j++) {
-        if (j > i) {
-        prim_c(i, j) = 1;
-        } else if (j == i) { // if last col
-        prim_c(i, j) = sin(phi[j]);
-        } else { // if not last col
-        prim_c(i, j) = cos(phi[j]);
-        }
+          if (j > i) {
+            prim_c(i, j) = 1;
+          } else if (j == i) { // if last col
+            prim_c(i, j) = sin(phi[j]);
+          } else { // if not last col
+            prim_c(i, j) = cos(phi[j]);
+          }
         }
       }
+    }
   }
-}
-      
-      // Collapse cols by multiplication to create c matrix
-      for (int i = 0; i < prim_c.n_rows; i++) {
-      double mult = 1;
-      
-      for (int j = 0; j < prim_c.n_cols; j++) {
+  
+  // Collapse cols by multiplication to create c matrix
+  for (int i = 0; i < prim_c.n_rows; i++) {
+    double mult = 1;
+    
+    for (int j = 0; j < prim_c.n_cols; j++) {
       mult = mult * prim_c(i, j);
-      }
-      
-      c(i, 0) = mult;
-      }
-      
-      return c;
+    }
+    
+    c(i, 0) = mult;
+  }
+  
+  return c;
 }
 
 arma::vec insZ (arma::vec vals) {
@@ -71,6 +77,9 @@ arma::vec insZ (arma::vec vals) {
 }
 
 arma::mat fillM (arma::vec vals) {
+  // Function to fill M given the vals.
+  // I could not figure out a simple way, hence
+  // this is done using an algorithm.
   int k = vals.n_elem;
   arma::mat M(k, k, arma::fill::zeros);
   arma::rowvec valsZ;
@@ -86,37 +95,43 @@ arma::mat fillM (arma::vec vals) {
 }
 
 arma::mat invBMat (int k) {
-    arma::mat B(k+1, k+1);
-    arma::mat invB(k+1, k+1);
-    arma::mat M(k+1, k+1);
-    arma::vec vals(k+1);
-    int MSize = (k+1) * (k+1);
-    
-    // M matrix values
-    if (k == 1) {
+  // Following the notation in Woods & Lin (2009)...
+  // Inverse of B is a frequently used term. B itself is defined such that
+  // B^T B = M.
+  // M is filled manually as it is given in Equation (9),
+  // fillM() is defined above for this purpose. Then, B is obtained
+  // by Cholesky decomposition and inversed.
+  arma::mat B(k+1, k+1);
+  arma::mat invB(k+1, k+1);
+  arma::mat M(k+1, k+1);
+  arma::vec vals(k+1);
+  int MSize = (k+1) * (k+1);
+  
+  // M matrix values
+  if (k == 1) {
     vals = arma::vec({1, 1});
-    } else if (k == 2) {
+  } else if (k == 2) {
     vals = arma::vec({1, 1, 3});
-    } else if (k == 3) {
-      vals = arma::vec({1, 1, 3, 15});
-    } else if (k == 4) {
-      vals = arma::vec({1, 1, 3, 15, 105});
-    } else if (k == 5) {
-      vals = arma::vec({1, 1, 3, 15, 105, 945});
-    } else if (k == 6) {
-      vals = arma::vec({1, 1, 3, 15, 105, 945, 10395});
-    } else if (k == 7) {
-      vals = arma::vec({1, 1, 3, 15, 105, 945, 10395, 135135});
-    } else if (k == 8) {
-      vals = arma::vec({1, 1, 3, 15, 105, 945, 10395, 135135, 2027025});
-    }
-    
-    // Fill M
-    M = fillM(vals);
-    
-    B = arma::chol(M);
-    invB = arma::inv(B);
-    
-    return invB;
-    }
+  } else if (k == 3) {
+    vals = arma::vec({1, 1, 3, 15});
+  } else if (k == 4) {
+    vals = arma::vec({1, 1, 3, 15, 105});
+  } else if (k == 5) {
+    vals = arma::vec({1, 1, 3, 15, 105, 945});
+  } else if (k == 6) {
+    vals = arma::vec({1, 1, 3, 15, 105, 945, 10395});
+  } else if (k == 7) {
+    vals = arma::vec({1, 1, 3, 15, 105, 945, 10395, 135135});
+  } else if (k == 8) {
+    vals = arma::vec({1, 1, 3, 15, 105, 945, 10395, 135135, 2027025});
+  }
+  
+  // Fill M
+  M = fillM(vals);
+  
+  B = arma::chol(M);
+  invB = arma::inv(B);
+  
+  return invB;
+}
 
